385929ea8827c766ba533b6e5f5d2ccf99199dbf
15framespersecond <grekcut@gmail.com>
2024-06-06 15:20:42 -0700

    fixed data folder

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+# untested
+
+# downloads SD data
 wget "https://zenodo.org/records/4487613/files/data.zip?download=1"
 unzip data.zip -d ../data/temp/
 rm -r data.zip
 mv ../data/temp/data/dataSD1500/* ../data/raw/
 mv ../data/temp/data/dataSD750/* ../data/raw/
 
-# note untested
+# downloads summary
+wget "https://zenodo.org/records/4487613/files/summary.zip?download=1"
+unzip data.zip -d ../data/temp/
+rm -r summary.zip
+mv ../data/temp/dataSummary* ../data/raw/
--- src/data/makedataset.py
+++ src/data/makedataset.py
@@ -8,31 +8,50 @@ import datetime
 import os
 import ipywidgets as widgets
 from ipywidgets import  interactive
-from tmp import import_files
-# Jupyter/ IPython formatting
-from IPython.display import Math, Latex, display
-
-# Default values for plots
-plt.rcParams["figure.figsize"] = [14, 9] # figure width and height
-plt.rcParams["font.size"] = 20
 
 
-            
+
+def import_file(file_path):
+    '''imports one file
+    '''
+    try:
+        with open(file_path, 'r') as file:
+            d = json.load(file)
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file {file_path}.")
+    return d            
 
 def get_signal_data(path):
+    '''Taking in the path of the project ex /home/tuckyg/Documents/PHYS-139-GROUP-PROJECT and returns signal data similar to previous hw
+    '''
     path = path + '/data/raw/dataSD1500/'
     files = os.listdir(path)
+    binsize = 0.025
     y = []
+    t=[]
     for f in files:
-        data = import_files(path+f)
+        data = import_file(path+f)
 
 #stations contain the signal of the 3 pmts of the wcds and some related parameters
         stations = pd.DataFrame(data['stations'])
         stations.set_index('id', inplace=True)
 
-
+        
     
         wcd = stations.iloc[0] #select the first station 
+
         # Assuming y is a list initialized earlier in your code
         y.append(wcd.pmt1)
-    return y
+        time = [np.arange(len(wcd[f'pmt{i + 1}'])) * binsize for i in range(3)]
+        t.append(time[0])
+    return y, t, data
+
+def VEM_to_eV(VEM):
+    '''slope and intercept are claculated using the energy-calibration.ipynb
+    '''
+    slope = 45.14153715340955
+    intercept = 2.3400270892667407
+    return (VEM - intercept) / slope
+
