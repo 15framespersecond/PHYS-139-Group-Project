d469377c63704e415672e9975c87b5c419036748
15framespersecond <grekcut@gmail.com>
2024-06-13 15:21:58 -0700

    added data generation

--- notebooks/data/makedataset.py
+++ notebooks/data/makedataset.py
@@ -25,6 +25,7 @@ def get_signal_data(path):
     files = os.listdir(path)
     binsize = 0.025
     y = []
+    t=[]
     data_to_save=[]
     for f in files:
         data = import_file(path+f)
@@ -40,8 +41,9 @@ def get_signal_data(path):
         # Assuming y is a list initialized earlier in your code
         y.append(wcd.pmt1)
         time = [np.arange(len(wcd[f'pmt{i + 1}'])) * binsize for i in range(3)]
-        data_to_save.append([wcd.pmt1])
-    return y, data_to_save
+        t.append(time[0])
+        data_to_save.append([time,wcd.pmt1])
+    return y, t, data_to_save
 
 def VEM_to_eV(VEM):
     VEM = np.array(VEM)
@@ -66,5 +68,30 @@ def bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter(order, [low, high], btype='band')
     y = filtfilt(b, a, data)
     return y / 10
+
+def pad_and_truncate(array, pad_length):
+    if pad_length < 0:
+        raise ValueError("pad_length must be a non-negative integer")
+    
+    if pad_length > len(array):
+        raise ValueError("pad_length cannot be greater than the length of the array")
+
+    # Create a new array with the specified padding and truncated length
+    padded_array = np.zeros_like(array)
+    padded_array[:pad_length] = 0
+    truncated_length = len(array) - pad_length
+    padded_array[pad_length:] = array[:truncated_length]
+
+    return padded_array
+
+def SNR(signal):
+    signal = np.array(signal)
+    clean_signal = signal[np.isfinite(signal)]
+    if clean_signal.size == 0:
+        return np.nan
+    
+    max = np.nanmax(clean_signal)
+    rms = np.sqrt(np.nanmean(clean_signal ** 2))
+    return max/rms if rms != 0 else np.nan
 
 
